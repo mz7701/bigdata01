fd3d2f825078b6868313c4582c7787388560329a
mz7701 <korean2071@naver.com>
2025-03-20 11:30:23 +0900

    v0.3 prime number

--- week03_prime.py
+++ week03_prime.py
@@ -1,9 +1,11 @@
 number = int(input())
 count =0
-for i in range(1, number+1):
-    if number % i == 0:
-        count = count + 1
 
+if number >= 2:
+
+    for i in range(2, number):
+        if number % i == 0:
+            count = count + 1
         print(i, end=" ")
 
     if count == 2:
